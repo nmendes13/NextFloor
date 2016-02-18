f5342437adac8bfdbaf77259d8d3ef7400b17593
nmendes <nelson.mendes.10@gmail.com>
2016-02-18 11:24:47 +0000

    First Commit

---
+++ NextFloorDB/Building.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Building]
+(
+	[BuidingID] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(10) NULL, 
+    [Street] NCHAR(10) NULL, 
+    [City] NCHAR(10) NULL, 
+    [PostCode] NCHAR(10) NULL
+)
