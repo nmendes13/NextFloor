550ecf358cab6560df5da3f5dbfdf25a90244409
nmendes <nelson.mendes.10@gmail.com>
2016-02-18 17:21:30 +0000

    templates

---
+++ NextFloorDAL/DBConnection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextFloorDAL
+{
+    public class DBConnection
+    {
+        private OdbcConnection connection;
+        public DBConnection(string connectionString)
+        {
+            //Data Source=(localdb)\ProjectsV12;Initial Catalog=NextFloorDB;Integrated Security=True;Pooling=False;Connect Timeout=30
+            connection = new OdbcConnection(connectionString);
+        }
+        public void OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+            }catch(Exception ex)
+            {
+
+            }
+        }
+        public void CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ NextFloorDB/Neighbour.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Neighbour]
+(
+	[NeighbourID] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [Email] NVARCHAR(50) NULL, 
+    [MobilePhoneNr] NVARCHAR(50) NULL
+)
