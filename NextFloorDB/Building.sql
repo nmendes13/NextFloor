2c0c1e3399a5f0c939778c7dcdf228d21662c17b
NelsonMendes <NelsonMendes@NELSONMENDES-PC>
2016-11-14 11:21:15 +0000

    Change

--- NextFloorDB/Building.sql
+++ NextFloorDB/Building.sql
@@ -2,7 +2,11 @@
 (
 	[BuidingID] INT NOT NULL PRIMARY KEY, 
     [Name] NCHAR(10) NULL, 
-    [Street] NCHAR(10) NULL, 
+    [StreetName] NCHAR(10) NULL, 
     [City] NCHAR(10) NULL, 
-    [PostCode] NCHAR(10) NULL
+    [PostCode] NCHAR(10) NULL, 
+    [HouseNumber] NCHAR(10) NULL, 
+    [State] NCHAR(10) NULL, 
+    [CountryID] INT NULL, 
+    [Floor] NCHAR(10) NULL
 )
---
+++ NextFloorDB/User.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[IdUser] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Email] VARCHAR(100) NOT NULL, 
+    [Password] VARCHAR(50) NOT NULL 
+)
